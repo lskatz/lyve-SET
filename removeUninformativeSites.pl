e3192c6ae131b25dc5cb6d3627b9399ef74f1497
Lee Katz <lkatz@cdc.gov>
2013-08-27 15:38:43 -0400

    reports every 1000 informative bp

--- removeUninformativeSites.pl
+++ removeUninformativeSites.pl
@@ -9,64 +9,67 @@ use Data::Dumper;
 use Getopt::Long;
 
 sub logmsg{$|++;print STDERR "@_\n"; $|--;}
-my $settings={};
-GetOptions($settings,qw(help verbose));
-die usage() if($$settings{help});
+exit main();
+sub main{
+  my $settings={};
+  GetOptions($settings,qw(help verbose));
+  die usage() if($$settings{help});
 
-## read in the fasta file into @seq and %seq, and keep the deflines
-my($length,$defline,@defline,@seq,%seq);
-while(<>){
-  chomp; 
-  if(/^>/){
-    s/>//; 
-    $defline=$_;
-    push(@defline,$defline);
-    next;
+  ## read in the fasta file into @seq and %seq, and keep the deflines
+  my($length,$defline,@defline,@seq,%seq);
+  while(<>){
+    chomp; 
+    if(/^>/){
+      s/>//; 
+      $defline=$_;
+      push(@defline,$defline);
+      next;
+    } 
+    $seq{$defline}=$_; 
+    push(@seq,$_); 
+    $length=length($_);
   } 
-  $seq{$defline}=$_; 
-  push(@seq,$_); 
-  $length=length($_);
-} 
 
-## read informative positions into @pos
-# compare each sequence to the reference sequence (the first sequence)
-my $refSeq=shift(@seq); 
-my $refId=shift(@defline); 
-my (%aln,@pos);
-my $numOtherSeq=@seq;
-my $informativeCount=0;
-POSITION:for(my $j=0;$j<$length;$j++){ 
-  my $informative=0; 
-  for(my $i=0;$i<$numOtherSeq;$i++){
-    my $sequence=$seq[$i];
-    my $nt=substr($sequence,$j,1); 
-    $informative=1 if($nt ne substr($refSeq,$j,1)); 
-    next POSITION if($nt=~/[nN\-]/); 
-  } 
-  next if(!$informative);
-  my $refNt=substr($refSeq,$j,1);
-  next if($refNt=~/[nN\-]/); # if it's informative, but the ref base isn't good, then skip
-  push(@pos,$j);
-  
-  $informativeCount++;
-  logmsg $j if($informativeCount % 1000 == 0 && $$settings{verbose});
-}
-
-## print all nucleotides found at informative positions
-# bring back the reference sequence
-unshift(@seq,$refSeq);
-unshift(@defline,$refId);
-for (my $i=0;$i<@seq;$i++){
-  my $sequence=$seq[$i];
-  my $id=$defline[$i];
-  print ">$id\n";
-  for my $pos(@pos){
-    print substr($sequence,$pos,1);
+  ## read informative positions into @pos
+  # compare each sequence to the reference sequence (the first sequence)
+  my $refSeq=shift(@seq); 
+  my $refId=shift(@defline); 
+  my (%aln,@pos);
+  my $numOtherSeq=@seq;
+  my $informativeCount=0;
+  POSITION:for(my $j=0;$j<$length;$j++){ 
+    my $informative=0; 
+    for(my $i=0;$i<$numOtherSeq;$i++){
+      my $sequence=$seq[$i];
+      my $nt=substr($sequence,$j,1); 
+      $informative=1 if($nt ne substr($refSeq,$j,1)); 
+      next POSITION if($nt=~/[nN\-]/); 
+    } 
+    next if(!$informative);
+    my $refNt=substr($refSeq,$j,1);
+    next if($refNt=~/[nN\-]/); # if it's informative, but the ref base isn't good, then skip
+    push(@pos,$j);
+    
+    $informativeCount++;
+    logmsg $j if($informativeCount % 1000 == 0 && $$settings{verbose});
   }
-  print "\n";
-}
 
-return 0;
+  ## print all nucleotides found at informative positions
+  # bring back the reference sequence
+  unshift(@seq,$refSeq);
+  unshift(@defline,$refId);
+  for (my $i=0;$i<@seq;$i++){
+    my $sequence=$seq[$i];
+    my $id=$defline[$i];
+    print ">$id\n";
+    for my $pos(@pos){
+      print substr($sequence,$pos,1);
+    }
+    print "\n";
+  }
+
+  return 0;
+}
 
 sub usage{
   "Removes all the uninformative sites in a multiple sequence alignment fasta file: Ns, gaps, and invariant sites
