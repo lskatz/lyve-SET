daa9701e72729eb53d190c3129f2772bc12cd3de
Lee Katz <lkatz@cdc.gov>
2013-08-27 10:59:48 -0400

    fixed a bug

--- launch_vcfToAlignment.sh
+++ launch_vcfToAlignment.sh
@@ -6,7 +6,7 @@ ref=$3
 out=$4
 
 if [ "$out" = "" ]; then
-  echo usage: $0 bam/ vcf/ ref.fasta badSites.txt out.aln.fas
+  echo usage: $0 bam/ vcf/ ref.fasta out.aln.fas
   echo "  bam directory's *.sorted.bam will be read"
   echo "  vcf dir's *.vcf will be read"
   echo "  vcfDir/*.badsites.txt will be used as 'bad sites', as produced by filterVcf.pl"
@@ -16,10 +16,13 @@ fi;
 echo "gathering $vcfDir/*.badsites.txt"
 bad="$vcfDir/allsites.txt"
 sort $vcfDir/*.badsites.txt | uniq > $bad
+if [ $? -gt 0 ]; then exit 1; fi;
 
 echo "vcfToAlignment.pl"
-vcfToAlignment.pl bam/*.sorted.bam vcf/*.vcf -o $out -r $ref -b $bad -a 20
+vcfToAlignment.pl $bamDir/*.sorted.bam $vcfDir/*.vcf -o $out -r $ref -b $bad -a 0 -n 8
+if [ $? -gt 0 ]; then exit 1; fi;
 
 echo "convertAlignment.pl"
 convertAlignment.pl -i $out -o $out.phy -f phylip -r
+if [ $? -gt 0 ]; then exit 1; fi;
 
