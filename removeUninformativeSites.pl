5d51fdaa37b1ea2da3d3917a71fc64520b5e0713
Lee Katz <lkatz@cdc.gov>
2013-09-27 11:40:01 -0400

    made the script faster; added comments; added options to keep gaps or to keep Ns

--- removeUninformativeSites.pl
+++ removeUninformativeSites.pl
@@ -12,8 +12,10 @@ sub logmsg{$|++;print STDERR "@_\n"; $|--;}
 exit main();
 sub main{
   my $settings={};
-  GetOptions($settings,qw(help verbose));
+  GetOptions($settings,qw(help verbose ambiguities-allowed gaps-allowed));
   die usage() if($$settings{help});
+  $$settings{"--ambiguities-allowed"} ||=0;
+  $$settings{"--gaps-allowed"} ||=0;
 
   ## read in the fasta file into @seq and %seq, and keep the deflines
   my($length,$defline,@defline,@seq,%seq);
@@ -32,32 +34,38 @@ sub main{
 
   ## read informative positions into @pos
   # compare each sequence to the reference sequence (the first sequence)
+  my $removeAmbiguities=!$$settings{"--ambiguities-allowed"};
+  my $removeGaps=!$$settings{"--gaps-allowed"};
   my $refSeq=shift(@seq); 
   my $refId=shift(@defline); 
   my (%aln,@pos);
   my $numOtherSeq=@seq;
   my $informativeCount=0;
   POSITION:for(my $j=0;$j<$length;$j++){ 
-    my $informative=0; 
+    my $refNt=substr($refSeq,$j,1);
+    next if($removeAmbiguities && uc($refNt) eq 'N'); # if it's informative, but the ref base isn't good, then skip
+    next if($removeGaps && $refNt eq '-');            # if it's informative, but the ref base isn't good, then skip
+
+    # see if the rest of this column shows that it is informative
+    my $informative=0; # guilty until proven innocent
     for(my $i=0;$i<$numOtherSeq;$i++){
-      my $sequence=$seq[$i];
-      my $nt=substr($sequence,$j,1); 
+      #my $sequence=$seq[$i];
+      my $nt=substr($seq[$i],$j,1); 
       die "ERROR: Sequence $i does not have a nucleotide at position $j! Is the MSA flush?" if(!$nt);
-      $informative=1 if($nt ne substr($refSeq,$j,1)); 
-      next POSITION if($nt=~/[nN\-]/); 
+      $informative=1 if($nt ne $refNt);  # It's informative, but you have to continue reviewing
+                                         # the other sequences if you are ignoring N or gap columns.
+      next POSITION if($removeAmbiguities && uc($nt) eq 'N'); # no chance of redemption here
+      next POSITION if($removeGaps && $nt eq '-');            # no chance of redemption here
     } 
     next if(!$informative);
-    my $refNt=substr($refSeq,$j,1);
-    next if($refNt=~/[nN\-]/); # if it's informative, but the ref base isn't good, then skip
-    push(@pos,$j);
+    push(@pos,$j); # retain this informative position
     
     $informativeCount++;
-    #logmsg $j if($informativeCount % 1000 == 0 && $$settings{verbose});
     logmsg $j if($$settings{verbose});
   }
 
-  ## print all nucleotides found at informative positions
-  # bring back the reference sequence
+  ## Print all nucleotides found at informative positions.
+  # Bring back the reference sequence.
   unshift(@seq,$refSeq);
   unshift(@defline,$refId);
   for (my $i=0;$i<@seq;$i++){
@@ -77,6 +85,10 @@ sub usage{
   "Removes all the uninformative sites in a multiple sequence alignment fasta file: Ns, gaps, and invariant sites
   Usage: $0 < aln.fasta > informative.fasta
   -v for verbose (technically makes the script slower)
+
+  Using the following two options will allow you to keep a master MSA list at-hand if you are converting all VCFs in a project
+  --gaps-allowed Allow gaps in the alignment
+  --ambiguities-allowed Allow ambiguous bases in the alignment
   "
 }
 
