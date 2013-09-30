5203ebae94372562b1a756d470b97d8c22a63bcb
Lee Katz <lkatz@cdc.gov>
2013-09-29 20:53:28 -0400

    better way to find the exec for phyml

--- launch_phyml.sh
+++ launch_phyml.sh
@@ -8,6 +8,11 @@ if [ "$aln" = "" ]; then
   exit 1;
 fi
 
-PhyML -i $aln -b -4 -m GTR -s BEST --quiet
+# find which phyml to use
+phyml=`(which phyml || which PhyML || which PhyML-3.1_linux64 || which phyml_linux_64) 2>/dev/null`;
+if [ $? -gt 0 ]; then echo "Could not find phyml"; exit 1; fi;
+echo "Found phyml at $phyml"
+
+$phyml -i $aln -b -4 -m GTR -s BEST --quiet
 if [ $? -gt 0 ]; then exit 1; fi;
 
