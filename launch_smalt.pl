c22be600699b7d7c26f4b21f588890b77db533e5
Lee Katz <lkatz@cdc.gov>
2013-10-28 17:30:45 -0400

    init

---
+++ launch_smalt.pl
@@ -0,0 +1,135 @@
+#!/usr/bin/env perl
+
+# qsub -N 'q$b' -cwd -V -o $log/$b.out -e $log/$b.out $scriptsdir/launch_smalt.sh $ref $fastq $bamdir/$b.bam $tmpdir
+
+use strict;
+use warnings;
+use Data::Dumper;
+use Getopt::Long;
+use File::Basename;
+use Bio::Perl;
+
+$0=fileparse $0;
+sub logmsg{print STDERR "@_\n";}
+exit(main());
+
+sub main{
+  my $settings={clean=>1};
+  GetOptions($settings,qw(help reference=s fastq=s bam=s tempdir=s clean! numcpus=i));
+  for(qw(reference fastq bam)){
+    die "ERROR: need option $_\n".usage() if(!$$settings{$_});
+  }
+  $$settings{numcpus}||=1;
+  $$settings{tempdir}||="tmp";
+
+  mkdir $$settings{tempdir} if(!-d $$settings{tempdir});
+
+  my $fastq=$$settings{fastq};
+  my $bam=$$settings{bam};
+  my $reference=$$settings{reference};
+
+  $fastq=cleanReads($fastq,$settings) if($$settings{clean});
+  mapReads($fastq,$bam,$reference,$settings);
+
+  return 0;
+}
+
+sub cleanReads{
+  my($query,$settings)=@_;
+  logmsg "Trimming, removing duplicate reads, and cleaning";
+  my $b=fileparse $query;
+  my $prefix="$$settings{tempdir}/$b";
+  system("run_assembly_trimLowQualEnds.pl -c 99999 $query > '$prefix.trimmed.fastq'"); 
+  die if $?;
+  system("run_assembly_removeDuplicateReads.pl '$prefix.trimmed.fastq' > '$prefix.nodupes.fastq'");
+  die if $?;
+  system("run_assembly_trimClean.pl -n $$settings{numcpus} -i '$prefix.nodupes.fastq' -o '$prefix.cleaned.fastq' --min_length 36");
+  die if $?;
+  system("rm -v '$prefix.nodupes.fastq' '$prefix.trimmed.fastq'");
+  die if $?;
+
+  return "$prefix.cleaned.fastq";
+}
+
+sub mapReads{
+  my($query,$bam,$ref,$settings)=@_;
+  return 1 if(-s "$bam.depth");
+
+  my $b=fileparse $query;
+  my $prefix="$$settings{tempdir}/$b";
+  
+  # don't allow underscores in the reference
+  my $numUnderscores=`grep '>' $ref | grep -c _`; chomp($numUnderscores);
+  if($numUnderscores > 0){
+    logmsg "ERROR: You cannot have deflines with underscores in them because of the way this script handles deflines internally.";
+    logmsg "Suggestion: change all underscores to dashes";
+    logmsg "  sed -ip 's/_/-/g' $ref # an example ";
+    logmsg "TODO: automate this step";
+    die;
+  }
+
+  my $RANDOM=rand(999999);
+  my $tmpOut="$bam.$RANDOM.tmp";
+  logmsg "$bam not found. I'm really doing the mapping now!\n  Outfile: $tmpOut";
+
+  # deshuffle the reads
+  logmsg "Deshuffling to $prefix.1.fastq and $prefix.2.fastq";
+  system("run_assembly_shuffleReads.pl -d '$query' 1>'$prefix.1.fastq' 2>'$prefix.2.fastq'");
+  die "Problem with deshuffling reads! I am assuming paired end reads." if $?;
+
+  # mapping
+  system("smalt map -r -1 -f samsoft -n $$settings{numcpus} $ref '$prefix.1.fastq' '$prefix.2.fastq' | samtools view -bS -T $ref - > $tmpOut");
+  die if $?;
+  system("rm -v '$prefix.1.fastq' '$prefix.2.fastq'"); die if $?;
+
+  logmsg "Transforming the output file with samtools";
+  my $sPrefix="$tmpOut.sorted";
+  my $sorted="$sPrefix.bam"; # samtools adds the bam later, so I want to keep track of it
+
+  logmsg "Sorting the temporary bam file into $sorted";
+  system("samtools sort $tmpOut $sPrefix"); die if $?;
+  system("samtools index $sorted"); die if $?;
+
+  logmsg "Getting the depth at each position in the sorted bam file";
+  # read the depth, but unfortunately it skips over zero-depth sites
+  my %depth;
+  open(BAMDEPTH,"samtools depth $sorted | ") or die "ERROR: could not open the bam file with samtools: $!";
+  while(<BAMDEPTH>){
+    chomp;
+    my @F=split /\t/;
+    $depth{$F[0]}{$F[1]}=$F[2];
+  }
+  close BAMDEPTH;
+ 
+  # get zero-depth information in there too. First find the total length of each contig.
+  open(FIXEDDEPTH,">","$sorted.depth") or die "ERROR: could not write to $sorted.depth: $!";
+  my %max;
+  my $in=Bio::SeqIO->new(-file=>$ref);
+  while(my $seq=$in->next_seq){
+    $max{$seq->id}=$seq->length;
+  }
+  for my $contig(keys(%max)){
+    for my $i(1..$max{$contig}){
+      $depth{$contig}{$i}||=0; 
+      print FIXEDDEPTH join("\t",$contig,$i,$depth{$contig}{$i})."\n";
+    }
+  }
+  close FIXEDDEPTH;
+  
+  system("mv -v $sorted $bam"); die if $?;
+  system("mv -v $sorted.bai $bam.bai"); die if $?;
+  system("mv -v $sorted.depth $bam.depth"); die if $?;
+  system("rm -v $tmpOut"); die if $?;
+
+  return 1;
+}
+
+sub usage{
+  "Maps a read set against a reference genome using smalt. Output file will be file.bam and file.bam.depth
+  Usage: $0 -f file.fastq -b file.bam -t tmp/ -r reference.fasta
+  --noclean if you don't want to clean the reads internally
+  -t tmp to set the temporary directory as 'tmp'
+  --numcpus 1 number of cpus to use
+  "
+}
+
