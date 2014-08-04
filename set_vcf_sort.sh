9f341edecbec242a41d94ba9cefdfd7c90ab9b8c
Lee Katz - Aspen <gzu2@cdc.gov>
2014-08-04 15:27:26 -0400

    error checking

--- set_vcf_sort.sh
+++ set_vcf_sort.sh
@@ -14,7 +14,15 @@ fi
 
 # Get all the headers first
 grep '^#' "$VCF"
+if [ $? -gt 0 ]; then 
+  echo "ERROR: could not run grep on $VCF";
+  exit 1; 
+fi;
 
 # For all the lines that aren't headers,
 # Sort them by the contig and position
 grep -v '^#' "$VCF" | sort -k1,1 -k2,2n 
+if [ $? -gt 0 ]; then 
+  echo "ERROR: could not run grep on $VCF";
+  exit 1; 
+fi;
