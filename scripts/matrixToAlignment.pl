56ac95759bc1de9ef7f6c641e1e96d67816eb413
Lee Katz - Aspen <gzu2@cdc.gov>
2014-09-30 15:28:05 -0400

    starting to force the pipeline to go through a matrix step before fasta step

--- scripts/matrixToAlignment.pl
+++ scripts/matrixToAlignment.pl
@@ -1,13 +1,13 @@
 #!/usr/bin/env perl
 
-# Removes uninformative sites from a fasta multiple sequence alignment: Ns, gaps, and invariant sites
+# Changes a SNP matrix to a MSA
 # Author: Lee Katz
 
 use strict;
 use warnings;
 use Data::Dumper;
 use Getopt::Long;
-use Bio::SeqIO;
+use Bio::Perl;
 use File::Basename;
 
 $0=fileparse $0;
@@ -18,23 +18,59 @@ sub main{
   GetOptions($settings,qw(help verbose min-distance=i format=s)) or die $!;
   die usage() if($$settings{help});
   $$settings{"min-distance"} ||=0;
-  $$settings{format}||="fasta";
+  $$settings{format}         ||="fasta";
 
   die usage() if($$settings{help});
 
-  my $seqHash=readMatrix($settings);
+  my $seqObj=readMatrix($settings);
+
+  printSeqs($seqObj,$settings);
 
   return 0;
 }
 
 sub readMatrix{
+  my ($settings)=@_;
+
+  # read either stdin or the filename
+  my $fh;
+  if(@ARGV == 1){
+    logmsg "Reading from $ARGV[0]";
+    open($fh,$ARGV[0]);
+  } elsif(@ARGV > 1){
+    die "ERROR: you can only have one filename but you gave multiples: ".join(", ",@ARGV)."\n".usage();
+  } else {
+    $fh=\*STDIN;
+    logmsg "Reading from stdin";
+  }
+
+  my $posHeader=<$fh>;
+  chomp($posHeader);
+  my ($dot,@pos)=split(/\t/,$posHeader);
+  my $seqObj;
+  while(<$fh>){
+    chomp;
+    my($genome,@nt)=split(/\t/,$_);
+    my $sequence=join("",@nt);
+    my $seq=Bio::Seq->new(-id=>$genome,seq=>$sequence);
+    push(@$seqObj,$seq);
+  }
   
+  close $fh;
+  return ($seqObj,\@pos) if wantarray;
+  return $seqObj;
+}
+
+sub printSeqs{
+  my($seq,$settings)=@_;
+  my $out=Bio::SeqIO->new(-format=>$$settings{format},-fh=>\*STDOUT);
+  return $out->write_seq(@$seq);
 }
 
 sub usage{
   "$0: turns a SNP matrix into an alignment
-  Usage: $0 matrix.tsv > aln.out.fas
-         $0 matrix.tsv | removeUninformativeSites.pl > informative.aln.fas
+  Usage: $0 < matrix.tsv > aln.out.fas
+         $0 < matrix.tsv | removeUninformativeSites.pl > informative.aln.fas
   --min-distance 0 The distance in bp between allowed SNPs. Does not count sites where all nt are the same.
   --format fasta   The output format
   "
