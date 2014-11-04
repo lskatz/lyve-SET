f1c164dfd03c8d461e5b47273f6d9b2abebe2de5
Lee Katz - Aspen <gzu2@cdc.gov>
2014-11-04 14:20:51 -0500

    names the sample appropriately in the VCF output

--- scripts/launch_varscan.pl
+++ scripts/launch_varscan.pl
@@ -6,19 +6,22 @@ use strict;
 use warnings;
 use Data::Dumper;
 use Getopt::Long;
-use File::Basename;
+use File::Basename qw/basename fileparse/;
 use Bio::Perl;
 use Vcf;
 
+my $samplename="Sample1"; # to be changed later
 $0=fileparse $0;
-sub logmsg{print STDERR "@_\n";}
+sub logmsg{print STDERR "$0: @_\n";}
 exit(main());
+ 
 
 sub main{
   my $settings={};
   GetOptions($settings,qw(help reference=s tempdir=s altFreq=s coverage=i));
 
   my $bam=$ARGV[0] or die "ERROR: need bam\n".usage();
+  $samplename=basename($bam,qw(.sorted.bam .bam));
   my $reference=$$settings{reference} or die "ERROR: need --reference\n".usage();
   $$settings{tempdir}||="tmp";
   $$settings{coverage}||=10;
@@ -49,14 +52,18 @@ sub varscan{
   my($pileup,$settings)=@_;
   die "ERROR: the pileup is a zero-byte file\n  $pileup" if(-s $pileup < 1);
   my $vcf="$$settings{tempdir}/".fileparse($pileup).".tmp.vcf.gz";
-  system("varscan.sh mpileup2cns $pileup --min-coverage $$settings{coverage} --min-coverage 10 --min-var-freq 0.75 --output-vcf 1 | bgzip -c > $vcf");
-  return $vcf;
+  system("varscan.sh mpileup2cns $pileup --min-coverage $$settings{coverage} --min-coverage 10 --min-var-freq 0.75 --output-vcf 1 |\
+    perl -lane 's/Sample1/\Q$samplename\E/; print;' |\
+    bgzip -c > $vcf
+  ");
   die if $?;
+
+  return $vcf;
 }
 
 sub backfillVcfValues{
   my($vcfFile,$bam,$settings)=@_;
-  logmsg "Backfilling values in $vcfFile";
+  logmsg "Backfilling values in $vcfFile and printing to stdout";
   my $vcf=Vcf->new(file=>$vcfFile);
 
   # alternate count column must be there (AC)
@@ -73,18 +80,18 @@ sub backfillVcfValues{
   while(my $x=$vcf->next_data_hash()){
     my $posId=$$x{CHROM}.':'.$$x{POS};
     my $pos=$$x{POS};
-    $$x{gtypes}{Sample1}{DP}=$$depth{$posId};
+    $$x{gtypes}{$samplename}{DP}=$$depth{$posId};
 
     # Alternate calls are in the AD field for varscan
     $vcf->add_format_field($x,"AC");
-    $$x{gtypes}{Sample1}{AD}||=0;
-    $$x{gtypes}{Sample1}{AC}||=$$x{gtypes}{Sample1}{AD};
+    $$x{gtypes}{$samplename}{AD}||=0;
+    $$x{gtypes}{$samplename}{AC}||=$$x{gtypes}{$samplename}{AD};
 
     # Put in exactly what the alternate call is
     $$x{ALT}[0]=$$x{REF} if($$x{ALT}[0] eq '.');
 
     # Figure out the genotype
-    #$$x{gtypes}{Sample1}{GT}=$$x{ALT}[0].'/'.$$x{ALT}[0];
+    #$$x{gtypes}{$samplename}{GT}=$$x{ALT}[0].'/'.$$x{ALT}[0];
 
     print $vcf->format_line($x);
   }
