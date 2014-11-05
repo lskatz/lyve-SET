9f935b8f226238ac2be7a1171bcd13a979d5f56f
Lee Katz - Aspen <gzu2@cdc.gov>
2014-11-05 11:06:57 -0500

    ambiguities treated more correctly

--- scripts/mvcfToAlignment.pl
+++ scripts/mvcfToAlignment.pl
@@ -8,11 +8,12 @@ use Bio::Perl;
 use File::Basename;
 
 $0=fileparse $0;
+sub logmsg{print STDERR "$0: @_\n";}
 
 exit main();
 sub main{
   my $settings={};
-  GetOptions($settings,qw(help));
+  GetOptions($settings,qw(help ambiguities));
   die usage() if($$settings{help});
   
   my $header=<>;
@@ -43,14 +44,21 @@ sub main{
     while(my($genome,$GT)=each(%F)){
       die "ERROR: could not find the genotype field (GT)!\n". Dumper [$genome,\%F] if(!$GT);
       my $nt=$GT;
-      if($nt=~/(.)[\/\|](.)/){
-        $nt=$1;
-        $nt="N" if($1 ne $2);
-        
-        # take care of indels or ambiguities
-        $nt="N" if(length($nt)!=1 || $nt!~/^[ATCG]$/i);
+      my ($nt1,$nt2)=split(/[\/\|]/,$nt);
+      if($nt1 eq $nt2){
+        $nt=$nt1;
+      } elsif($$settings{'ambiguities'}){
+        $nt=rev_iub([$nt1,$nt2],$settings);
+        logmsg "Warning: GT $GT was interpreted as an ambiguity $nt at $genome/$extra{CHROM}/$extra{POS}";
+      } else{
+        $nt="N";
       }
+
+      # ALT is defined as REF if it is a dot
       $nt=$extra{REF} if($nt eq '.' || $nt eq ',');
+      # take care of indels or ambiguities
+      $nt="N" if(length($nt)!=1 || $nt!~/^\w$/i);
+
       $matrix{$genome}{$extra{CHROM}}{$extra{POS}}=$nt;
     }
   }
@@ -79,9 +87,43 @@ sub main{
   return 0;
 }
 
+sub rev_iub{
+  my($ntArr,$settings)=@_;
+  die "ERROR: ntArr is not an array here:\n".Dumper $ntArr if(ref($ntArr ne "ARRAY"));
+  return $$ntArr[0] if(@$ntArr==1);
+
+  # Figure out the ambiguity now
+  # from bioperl, http://doc.bioperl.org/bioperl-live/Bio/Tools/IUPAC.html
+  my %REV_IUB = (
+          A    => 'A',
+          T    => 'T',
+          U    => 'U',
+          C    => 'C',
+          G    => 'G',
+          AC   => 'M',
+          AG   => 'R',
+          AT   => 'W',
+          CG   => 'S',
+          CT   => 'Y',
+          GT   => 'K',
+          ACG  => 'V',
+          ACT  => 'H',
+          AGT  => 'D',
+          CGT  => 'B',
+          ACGT => 'N',
+          N    => 'N'
+      );
+
+  my $sortedNt=join("",sort{$a cmp $b} @$ntArr);
+  my $ambiguity=$REV_IUB{$sortedNt};
+  die "ERROR: could not understand nt for ambiguities!\n".Dumper($ntArr) if(!$ambiguity);
+  return $ambiguity;
+}
+
 sub usage{
   "Multiple VCF format to alignment
   $0: reads a bcftools query output and creates a multiple sequence alignment file. Required columns: GT (genotype), REF, CHROM, POS
   Usage: bcftools query [...] | $0 > aln.fasta
+  --ambiguities  to allow for ambiguity letter codes other than 'N'
   "
 }
