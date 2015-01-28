8bdf77d080244727b9e9149fa33b2f71b17656ff
Lee Katz - Aspen <gzu2@cdc.gov>
2015-01-28 11:50:06 -0500

    checking for empty string

--- scripts/set_downloadTestData.pl
+++ scripts/set_downloadTestData.pl
@@ -76,6 +76,7 @@ sub downloadDataset{
   mkdir("$testdir/reads");
   for(@readsRemote){
     my ($r,$realname)=split(/\t/,$_);
+    $r||=""; $realname||="";
     $_=~s/^\s+|\s+$//g for($r,$realname);
     $realname=$r if(!$realname);
     downloadReads($r,$realname,$testdir,$settings);
