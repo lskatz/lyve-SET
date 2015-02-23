d21d19689679181fb9648c8337b168a98775dbc9
Lee Katz - Aspen <gzu2@cdc.gov>
2015-02-23 13:14:45 -0500

    bcf matrix to alignment works

--- scripts/matrixToAlignment.pl
+++ scripts/matrixToAlignment.pl
@@ -14,41 +14,20 @@ sub logmsg{print STDERR "$0: @_\n";}
 
 exit main();
 sub main{
-  my $settings={filter=>1};
-  GetOptions($settings,qw(help ambiguities! tempdir=s allowed=i prefix=s filter!)) or die $!;
+  my $settings={};
+  GetOptions($settings,qw(help tempdir=s)) or die $!;
   die usage() if($$settings{help} || !@ARGV);
   $$settings{tempdir}||="tmp";
-  $$settings{allowed}||=0;
 
-  # output files
-  #$$settings{prefix}||="$0.out";
-  #$$settings{bcfOutput}||="$$settings{prefix}.bcfquery";
-  #$$settings{filteredBcf}||="$$settings{prefix}.filtered.bcfquery";
-  #$$settings{fasta}   ||="$$settings{prefix}.aln.fas";
-  #$$settings{positions}||="$$settings{prefix}.pos";
+  my($matrix)=@ARGV;
 
-  #my $vcf=$ARGV[0];
-
-  # read in the file with bcftools query
-  #logmsg "Running bcftools query to find all SNPs";
-  #my %matrix;
-  #my $bcfqueryFile=bcftoolsQuery($vcf,$settings);
-
-  # Remove clustered SNPs, etc, unless the user doesn't want it
-  my $filteredMatrix=$bcfqueryFile;
-  if($$settings{filter}){
-    logmsg "Filtering for clustered SNPs or any other specified filters";
-    $filteredMatrix=filterSites($bcfqueryFile,$settings);
-  }
-
-  my $fasta=bcfqueryToFasta($filteredMatrix,$settings);
+  bcfqueryToFasta($matrix,$settings);
 
   return 0;
 }
 
 sub bcfqueryToFasta{
   my($bcfqueryFile,$settings)=@_;
-  my $outfile=$$settings{fasta};
   
   open(BCFQUERY,$bcfqueryFile) or die "ERROR: could not open $bcfqueryFile: $!";
 
@@ -106,6 +85,7 @@ sub bcfqueryToFasta{
       logmsg "Finished calling $i SNPs";
     }
   }
+  close BCFQUERY;
   # DONE getting it into memory!
 
   # START output
@@ -120,7 +100,7 @@ sub bcfqueryToFasta{
 
   # create the alignment
   logmsg "Found all positions; converting to fasta";
-  my $out=Bio::SeqIO->new(-file=>">$outfile",-format=>"fasta");
+  my $out=Bio::SeqIO->new(-format=>"fasta");
   for my $genome(@genome){
     my $seq="";
     for(@POS){
@@ -132,15 +112,7 @@ sub bcfqueryToFasta{
   }
   $out->close;
 
-  if($$settings{positions}){
-    logmsg "Sorted positions pertaining to the final (and possibly filtered) alignment are found in $$settings{positions}";
-    open(POS,">",$$settings{positions}) or die "ERROR: Could not open positions file $$settings{positions}: $!";
-    print POS "$_\n" for(@POS);
-    #print POS join("\n",@POS)."\n";
-    close POS;
-  }
-  
-  return 0;
+  return 1;
 }
 
 sub rev_iub{
@@ -176,92 +148,12 @@ sub rev_iub{
   return $ambiguity;
 }
 
-sub bcftoolsQuery{
-  my($file,$settings)=@_;
-  my $matrix="$$settings{tempdir}/$$.matrix";
-  my $bcfquery="bcftools query -i '%TYPE=\"snp\"' -f '%CHROM\\t%POS\\t%REF\\t[%TGT\\t]\\n' --print-header";
-  #my $bcfquery="bcftools query -i '%TYPE=\"snp\" && %MIN(DP)>=$$settings{min_coverage}' -f '%CHROM\\t%POS\\t%REF\\t[%TGT\\t]\\n' --print-header";
-
-  # handle the input file depending on how it is compressed or not
-  my $streamInCommand="";
-  if($file=~/\.gz$/){
-    $streamInCommand="gunzip -c $file";
-  } else {
-    $streamInCommand="cat $file";
-  }
-
-  # Open a bcftools query stream
-  open(QUERYSTREAM,"$streamInCommand | $bcfquery |") or die "ERROR running command: $!\n  $bcfquery < $file";
-  # Write the bcf query to a file
-  open(BCFQUERY,">",$$settings{bcfOutput}) or die "ERROR: could not open $$settings{bcfOutput} for writing: $!";
-  while(<QUERYSTREAM>){
-    print BCFQUERY $_;
-  }
-  close BCFQUERY;
-  close QUERYSTREAM;
-
-  # return the file instead of a large string
-  return $$settings{bcfOutput};
-}
-
-# Filter the BCF query file into a new one, if any filters were given
-sub filterSites{
-  my($bcfqueryFile,$settings)=@_;
-
-  my $outfile=$$settings{filteredBcf};
-  open(FILTEREDOUT,">",$outfile) or die "ERROR: could not open filtered output bcf query file: $!";
-
-  open(BCFQUERY,"<",$bcfqueryFile) or die "ERROR: could not open bcftools query file $bcfqueryFile for reading: $!";
-
-  # Read in the header with genome labels
-  my $header=<BCFQUERY>;
-  print FILTEREDOUT $header;  # Print the header right away so that it can be saved correctly before it's changed
-  $header=~s/^\s+|^#|\s+$//g; # trim and remove pound sign
-  my @header=split /\t/, $header;
-
-  # Read the actual content with variant calls
-  my ($currentPos,$currentChr);
-  while(my $bcfMatrixLine=<BCFQUERY>){
-
-    # get the fields from the matrix
-    my($CONTIG,$POS,$REF,@GT)=split(/\t/,$bcfMatrixLine);
-    
-    # get the values of the contig/pos if they aren't set
-    if(!defined($currentChr) || $CONTIG ne $currentChr){
-      # If the contig's value has switched, then undefine the position and start comparing on the new contig
-      undef($currentChr);
-      undef($currentPos);
-
-      # start anew with these coordinates
-      $currentChr=$CONTIG;
-      $currentPos=$POS;
-      seek(BCFQUERY,-length($bcfMatrixLine),1);
-      next;
-    }
-
-    # If the SNP is far enough away, then accept it into the new matrix
-    if($POS - $currentPos >= $$settings{allowed}){
-      print FILTEREDOUT $bcfMatrixLine;
-    }
-
-    # update the position
-    $currentPos=$POS;
-  }
-  close BCFQUERY;
-
-  close FILTEREDOUT;
-  return $outfile;
-}
-
 sub usage{
   "Multiple VCF format to alignment
-  $0: reads a pooled vcf (from bcftools merge) and creates a multiple sequence alignment file.
+  $0: reads a tab-delimted file created by 'bcftools query'.
   Usage: 
-    $0 pooled.fastq.gz --prefix out
-  --ambiguities       to allow for ambiguity letter codes other than 'N'
-  --nofilter          Do not filter out any SNPs (overrides --ambiguities)
-  --allowed 0         How close SNPs can be from each other before being thrown out
+    $0 bcfmatrix.tsv > alignment.fasta
+    $0 < bcfmatrix.tsv > alignment.fasta # read stdin
   --tempdir tmp       temporary directory
-  --prefix ./prefix   The prefix for all output files
   "
 }
