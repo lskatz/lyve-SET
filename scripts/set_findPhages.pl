1e0ea33f545ec46fd02382fb516da1b25f3f384a
Lee Katz - Aspen <gzu2@cdc.gov>
2015-04-09 11:57:59 -0400

    better phage parallelization

--- scripts/set_findPhages.pl
+++ scripts/set_findPhages.pl
@@ -7,6 +7,7 @@ use strict;
 use Getopt::Long;
 use Data::Dumper;
 use File::Basename;
+use File::Temp qw/tempdir/;
 use List::MoreUtils qw(uniq);
 use FindBin;
 use Bio::Perl;
@@ -17,12 +18,14 @@ sub logmsg{print STDERR "$0: @_\n";}
 exit main();
 sub main{
   my $settings={};
-  GetOptions($settings,qw(help species=s numcpus=i));
+  GetOptions($settings,qw(help species=s numcpus=i tempdir=s));
   $$settings{numcpus}||=1;
+  $$settings{tempdir}||=tempdir("phastXXXXXX",CLEANUP=>1,TMPDIR=>1);
 
   my $fasta=$ARGV[0];
   die usage() if(!$fasta || $$settings{help});
 
+  logmsg "Tempdir is $$settings{tempdir}";
   my $phastCoordinates=phast($fasta,$settings);
 
   my %seq=readFasta($fasta,$settings);
@@ -48,11 +51,31 @@ sub main{
 sub phast{
   my($fasta,$settings)=@_;
 
+  my $tempdir=tempdir("$$settings{tempdir}/phastXXXXXX",CLEANUP=>1,PREFIX=>$$settings{tempdir});
   my $db="$FindBin::RealBin/../lib/phast/phast.faa";
   logmsg "Running blastx against $db";
-  my $allResults=`blastx -query '$fasta' -db $db -evalue 0.05 -outfmt 6 -num_threads $$settings{numcpus}`;
-  #die $allResults;
+
+  # Better parallelization: one fasta entry per cpu.
+  # Split the query into multiple files and then figure out
+  # how many cpus per blast job we need.
+  my $i=0;
+  my $seqin=Bio::SeqIO->new(-file=>$fasta);
+  while(my $seq=$seqin->next_seq){
+    my $file="$tempdir/$i.fna";
+    open(SEQOUT,">",$file) or die "ERROR: could not write seq to temp file $file: $!";
+    print SEQOUT join("\n",">".$seq->id,$seq->seq,"");
+    close SEQOUT;
+    $i++;
+  }
+  my $threadsPerBlast=int($$settings{numcpus}/$i);
+  $threadsPerBlast=1 if($threadsPerBlast<1);
+
+  # Perform blast on these split files.
+  system("ls $tempdir/*.fna | xargs -I {} -P $$settings{numcpus} -n 1 blastx -query {} -db $db -evalue 0.05 -outfmt 6 -num_threads $threadsPerBlast -out {}.bls");
   die "ERROR with blastx: $!" if $?;
+  #my $allResults=`blastx -query '$fasta' -db $db -evalue 0.05 -outfmt 6 -num_threads $$settings{numcpus}`;
+  my $allResults=`cat $tempdir/*.fna.bls`;
+  die "ERROR with cat on $tempdir/*.fna.bls" if($? || !$allResults);
 
   logmsg "Parsing results";
   my(@range);
@@ -160,5 +183,6 @@ sub usage{
   "Finds phages in a fasta file using phast and PhiSpy
   Usage: $0 file.fasta
   --numcpus 1
+  --tempdir tmp/
   "
 }
