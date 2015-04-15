e91bd957e1d3df352414f5ac77b53da72a467d60
Lee Katz - Aspen <gzu2@cdc.gov>
2015-04-15 11:57:36 -0400

    some refactoring with Number::Range

--- scripts/set_findPhages.pl
+++ scripts/set_findPhages.pl
@@ -8,17 +8,20 @@ use Getopt::Long;
 use Data::Dumper;
 use File::Basename;
 use File::Temp qw/tempdir/;
+use List::Util qw/min max/;
 use List::MoreUtils qw(uniq);
-use FindBin;
 use Bio::Perl;
 
-local $0=fileparse $0;
-sub logmsg{print STDERR "$0: @_\n";}
+use FindBin;
+use lib "$FindBin::RealBin/../lib";
+use LyveSET qw/logmsg/;
+use Number::Range;
 
+local $0=fileparse $0;
 exit main();
 sub main{
   my $settings={};
-  GetOptions($settings,qw(help species=s numcpus=i tempdir=s));
+  GetOptions($settings,qw(help numcpus=i tempdir=s));
   $$settings{numcpus}||=1;
   $$settings{tempdir}||=tempdir("phastXXXXXX",CLEANUP=>1,TMPDIR=>1);
 
@@ -26,25 +29,12 @@ sub main{
   die usage() if(!$fasta || $$settings{help});
 
   logmsg "Tempdir is $$settings{tempdir}";
-  my $phastCoordinates=phast($fasta,$settings);
-
-  my %seq=readFasta($fasta,$settings);
-
-  my $ranges=findRangeUnion(\%seq,$phastCoordinates,$settings);
+  my $ranges=phast($fasta,$settings);
 
   # Print the ranges to stdout
   for my $r(@$ranges){
     print join("\t",@$r)."\n";
   }
-  return 0;
-
-  # print the masked sequences
-  while(my ($id,$seq)=each(%seq)){
-    $seq=~s/(.{60})/$1\n/g;
-    chomp($seq);
-    print ">$id\n$seq\n";
-  }
-
   return 0;
 }
 
@@ -78,91 +68,28 @@ sub phast{
   die "ERROR with cat on $tempdir/*.fna.bls" if($? || !$allResults);
 
   logmsg "Parsing results";
-  my(@range);
+  my(%range);
   for my $result(split(/\n/,$allResults)){
     $result=~s/^\s+|\s+$//g; # trim
     my ($contig,$hit,$identity,$length,$gaps,$mismatches,$sstart,$send,$qstart,$qend,$e,$score)=split /\t/, $result;
     next if($score < 50 || $length < 20);
     
-    push(@range,"$contig:$qstart-$qend");
-  }
-  @range=uniq(@range);
-  @range=sort{
-    my($contigA,$rangeA)=split(/:/,$a);
-    my($startA,$stopA)=split(/\-/,$rangeA);
-    my($contigB,$rangeB)=split(/:/,$b);
-    my($startB,$stopB)=split(/\-/,$rangeB);
-
-    return $contigA cmp $contigB if($contigA ne $contigB);
-    $startA<=>$startB; 
-  } @range;
-
-  return \@range;
-}
-
-sub findRangeUnion{
-  my($seq,$ranges,$settings)=@_;
-
-  my $points=simplifyRangesToPoints($ranges,$settings);
-  return pointsToRanges($points,$settings);
-  
-}
-
-sub simplifyRangesToPoints{
-  my($ranges,$settings)=@_;
-  
-  # 1) Convert to a better format
-  # 2) Find the union as we go
-  my (%unionIndex);
-  my %range;
-  for my $r(@$ranges){
-    my($contig,$range)=split(/:/,$r);
-    my($start,$stop)=split(/\-/,$range);
-    $start--; $stop--;
+    # Add these coordinates to ranges
+    $range{$contig}||=Number::Range->new;
+    my $lo=min($qstart,$qend);
+    my $hi=max($qstart,$qend);
     
-    # Convert the range to a true perl range, with a contig prefix
-    my @range=($start..$stop);
-    $_=$contig.":".$_ for(@range);
-
-    # Index the range
-    @unionIndex{@range} = (1) x @range;
+    no warnings;
+    $range{$contig}->addrange($lo..$hi);
   }
-  
-  # sort the points by contig/pos
-  my @union=sort {
-    my($contigA,$posA)=split(/:/,$a);
-    my($contigB,$posB)=split(/:/,$b);
-    return $contigA cmp $contigB if($contigA ne $contigB);
-    return $posA<=>$posB;
-  } keys(%unionIndex);
-  return \@union;
-}
 
-# Points should already be sorted
-sub pointsToRanges{
-  my($points,$settings)=@_;
+  # Translate the ranges found in the Range objects into 
+  # an array of [contig,start,stop]
   my @range;
-  my ($currentContig,$start)=split(/:/,$$points[0]);
-  my $end=$start;
-  my $numPoints=@$points;
-  for(my $i=1;$i<$numPoints-1;$i++){
-    my($contig,$pos)=split(/:/,$$points[$i]);
-    my($lastContig,$lastPos)=split(/:/,$$points[$i-1]);
-    my($nextContig,$nextPos)=split(/:/,$$points[$i+1]);
-
-    # The range ends if the next integer is not subsequent.
-    if($contig ne $lastContig || $pos-$lastPos!=1){
-      $end=$lastPos; # cut off the range
-
-      push(@range,[$currentContig,$start,$end]);
-
-      $currentContig=$contig;
-      $start=$pos;
-    }
-
-    # Record one final range at the end
-    if($i==$numPoints-2 && $contig eq $nextContig){
-      push(@range,[$nextContig,$start,$nextPos]);
+  while(my($contig,$rangeObj)=each(%range)){
+    my $rangeStr=$rangeObj->range;
+    while($rangeStr=~/(\d+)\.\.(\d+),?/g){
+      push(@range,[$contig,$1,$2]);
     }
   }
 
