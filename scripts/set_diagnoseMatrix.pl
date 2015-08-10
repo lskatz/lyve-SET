2ca62a80c27ee87f28ede3a73b41a5ba2990d1de
Lee - spacestation4 <gzu2@cdc.gov>
2015-08-10 18:11:27 -0400

    some progress

--- scripts/set_diagnoseMatrix.pl
+++ scripts/set_diagnoseMatrix.pl
@@ -13,21 +13,94 @@ exit(main());
 
 sub main{
   my $settings={};
-  GetOptions($settings,qw(help));
+  GetOptions($settings,qw(help reference=s));
   $$settings{maskedThresholdPercent}=10;
 
-  die usage if($$settings{help} || !@ARGV);
+  die usage() if($$settings{help} || !@ARGV);
 
+  my $refInfo=readAssembly($settings);
   for my $matrix(@ARGV){
     reportMaskedGenomes($matrix,$settings);
   }
 }
 
+sub readAssembly{
+  my($settings)=@_;
+  my $ref=$$settings{reference} || return {};
+
+  # run assembly metrics on min=150, 250, and 1000 to reflect different meanings
+}
+
 sub reportMaskedGenomes{
   my($matrix,$settings)=@_;
   open(MATRIX,$matrix) or die "ERROR: could not open $matrix for reading: $!";
-  while(<MATRIX>){
-    
+  my $header=<MATRIX>;
+  $header=~s/^#//g;                # remove leading pound sign
+  $header=~s/^\s+|\s+$//g;         # remove whitespace 
+  my @header=split(/\t/,$header);
+  $_=~s/^\[\d+\]// for(@header);   # remove leading col counter
+  $_=~s/:GT// for(@header);        # remove following :GT notation
 
+  # Read the SNP matrix file
+  my %maskCounter=();
+  my $numSites=0;
+  my($maskedSitesCounter, $masked50, $masked100)=(0,0,0);
+  while(<MATRIX>){
+    chomp;
+    my(%sample,%site);
+
+    # index the columns
+    my @F=split(/\t/);
+    @sample{@header}=@F;
+
+    # split that index into site info and sample info
+    for(qw(CHROM POS REF)){
+      $site{$_}=$sample{$_};
+      delete($sample{$_});
+    }
+    my $REF=$site{REF};
+    
+    my $numSamples=scalar(keys(%sample)); # TODO get this outside of the loop
+    my $numSamplesMaskedHere=0;
+    while(my($sample,$nt)=each(%sample)){
+      $nt=~s/\./$REF/;
+      # Count masked bases
+      if($nt=~/N/i){
+        $maskCounter{$sample}++;
+        $numSamplesMaskedHere++;
+      }
+    }
+
+    $maskedSitesCounter++ if($numSamplesMaskedHere>0);
+    $masked50++ if($numSamplesMaskedHere/$numSamples >= 0.5);
+    $masked100++ if($numSamplesMaskedHere/$numSamples == 1);
+
+    # Keep a running tally of sites
+    $numSites++;
+  }
   close MATRIX;
+
+  # Percentage of masked bases per genome
+  my @genome=sort { $a cmp $b } keys(%maskCounter); # sort, for prettier output
+  for my $sample(@genome){
+    my $count=$maskCounter{$sample};
+    my $percentGenomeMasked=int($count/$numSites*10000)/100;
+    logmsg "$sample is $percentGenomeMasked% masked" if($percentGenomeMasked > 10);
+  }
+
+  # How many sites are masked
+  my $percentMasked   =int($maskedSitesCounter/$numSites*10000)/100;
+  my $percentMasked50 =int($masked50/$numSites*10000)/100;
+  my $percentMasked100=int($masked100/$numSites*10000)/100;
+  logmsg "Sites masked at all: $percentMasked%";
+  logmsg "Sites where 50% of the site is masked: $percentMasked50%";
+  logmsg "Sites where 100% of the site is masked: $percentMasked100%";
+}
+
+sub usage{
+  "$0: diagnoses a SNP matrix file
+  Usage: $0 snpmatrix.tsv
+  NOTE: snpmatrix.tsv must have as the first three columns: CHROM, POS, REF and must have subsequent columns pertaining to each sample
+  -r reference.fasta (optional)
+  "
 }
