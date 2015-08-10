52197599b9eeab07aa526813da73608c205204d7
Lee Katz - Aspen <gzu2@cdc.gov>
2015-08-10 16:17:34 -0400

    post-processing matrix for homozygous calls

--- scripts/pooledToMatrix.sh
+++ scripts/pooledToMatrix.sh
@@ -9,6 +9,11 @@ usage () {
   return 0;
 }
 
+logmsg () {
+  echo "$script: $@";
+  return 0;
+}
+
 while getopts "ho:" o; do
   case "${o}" in
     h)
@@ -32,11 +37,31 @@ if [ "$OUT" == "" ] || [ "$IN" == "" ]; then
   exit 1;
 fi
 
-
-command="bcftools query -i '%TYPE=\"snp\"' -f '%CHROM\\t%POS\\t%REF\\t[%TGT\\t]\\n' --print-header $IN > $OUT.tmp && mv -v $OUT.tmp $OUT"
+# Create the basic matrix
+command="bcftools query -i '%TYPE=\"snp\"' -f '%CHROM\\t%POS\\t%REF\\t[%TGT\\t]\\n' --print-header $IN > $OUT.unrefined.tmp"
+logmsg $command;
 eval $command
 if [ $? -gt 0 ]; then
   echo -e "ERROR with bcftools:\n  $command";
   exit 1
 fi
 
+# post-process the matrix
+logmsg "Post-processing"
+perl -lane '
+  BEGIN{
+    # print the header
+    $line=<>;
+    chomp($line);
+    print $line;
+    $numFields=scalar(split(/\t/,$line));
+    $lastIndex=$numFields-1;
+  }
+  for(@F[3..$lastIndex]){
+    $_=substr($_,0,1);
+  }
+  print join("\t",@F);
+  ' < $OUT.unrefined.tmp > $OUT.tmp;
+
+mv -v $OUT.tmp $OUT;
+rm -v $OUT.unrefined.tmp;
