278e637cd928b2dfd3ee58ad6234fef7912eb8fd
Lee Katz - Aspen <gzu2@cdc.gov>
2015-08-11 16:37:41 -0400

    temporary directory now an option

--- scripts/mergeVcf.sh
+++ scripts/mergeVcf.sh
@@ -4,13 +4,16 @@
 script=$(basename $0);
 
 NUMCPUS=1; # default number of cpus
-while getopts "o:n:" o; do
+while getopts "o:n:t:" o; do
   case "${o}" in
     o)
       OUT="$OPTARG"
       ;;
     n)
       NUMCPUS="$OPTARG"
+      ;;
+    t)
+      TEMPDIR="$OPTARG"
       ;;
     *)
       echo "ERROR: I do not understand option $OPTARG"
@@ -24,6 +27,7 @@ if [ "$IN" == "" ] || [ "$OUT" == "" ]; then
   echo "$script: merges VCF files that are compressed with bgzip and indexed with tabix"
   echo "Usage: $script -o pooled.vcf.gz *.vcf.gz"
   echo "  -o output.vcf.gz  The output compressed VCF file"
+  echo "  -t tempdir        A temporary directory (Default: a random dir using mktemp)"
   exit 1;
 fi
 
@@ -32,95 +36,74 @@ fi
 REGION=$(echo "$IN" | xargs -P 1 -n 1 tabix -l | sort | uniq);
 
 # Multithread, one thread per region
-TMPDIR=$(mktemp -d /tmp/mergeVcf.XXXXXX);
-echo "$script: temporary directory is $TMPDIR";
+if [ "$TEMPDIR" == "" ]; then
+  TEMPDIR=$(mktemp -d /tmp/mergeVcf.XXXXXX);
+fi
+echo "$script: temporary directory is $TEMPDIR";
 echo "$script: Running bcftools merge";
 export IN;
-echo "$REGION" | xargs -P $NUMCPUS -n 1 -I {} bash -c 'bcftools merge --regions "{}" $IN --force-samples -o '$TMPDIR'/merged.$$.vcf;'
+echo "$REGION" | xargs -P $NUMCPUS -n 1 -I {} bash -c 'bcftools merge --regions "{}" $IN --force-samples -o '$TEMPDIR'/merged.$$.vcf;'
 if [ $? -gt 0 ]; then
   echo "$script: ERROR with bcftools merge"
-  rm -rvf $TMPDIR;
+  rm -rvf $TEMPDIR;
   exit 1;
 fi
 
 echo "$script: Concatenating vcf output"
-bcftools concat $TMPDIR/merged.*.vcf > $TMPDIR/concat.vcf
+bcftools concat $TEMPDIR/merged.*.vcf > $TEMPDIR/concat.vcf
 if [ $? -gt 0 ]; then
   echo "$script: ERROR with bcftools concat"
-  rm -rvf $TMPDIR;
+  rm -rvf $TEMPDIR;
   exit 1;
 fi
 
-echo "$script: parsing $TMPDIR/concat.vcf for high-quality positions"
-perl -lane '
-    if(/^#/){
-      print;
-    }elsif($F[4] ne "N"){ 
-      print; 
-    }else { 
-      $numF=@F; 
-      $has_hq=0; 
-      for my $info(@F[9..$numF-1]){ 
-        $gt=substr((split(/:/,$info))[0],0,1); 
-        $has_hq=1 if($gt=~/[ATCG0\.,]/i);
-      } 
-      print if($has_hq); 
-    }
-' < $TMPDIR/concat.vcf > $TMPDIR/hqPos.vcf
+#echo "$script: parsing $TEMPDIR/concat.vcf for high-quality positions"
+#perl -lane '
+#    if(/^#/){
+#      print;
+#    }elsif($F[4] ne "N"){ 
+#      print; 
+#    }else { 
+#      $numF=@F; 
+#      $has_hq=0; 
+#      for my $info(@F[9..$numF-1]){ 
+#        $gt=substr((split(/:/,$info))[0],0,1); 
+#        $has_hq=1 if($gt=~/[ATCG0\.,]/i);
+#      } 
+#      print if($has_hq); 
+#    }
+#' < $TEMPDIR/concat.vcf > $TEMPDIR/hqPos.vcf
+#if [ $? -gt 0 ]; then
+#  echo "$script: ERROR with perl parsing"
+#  rm -rvf $TEMPDIR;
+#  exit 1;
+#fi
+
+echo "$script: parsing $TEMPDIR/concat.vcf for SNPs"
+bcftools annotate --include '%TYPE="snp"' < $TEMPDIR/concat.vcf > $TEMPDIR/hqPos.vcf
 if [ $? -gt 0 ]; then
-  echo "$script: ERROR with perl parsing"
-  rm -rvf $TMPDIR;
+  echo "$script: ERROR with bcftools annotate"
+  rm -rvf $TEMPDIR;
   exit 1;
 fi
 
-bgzip $TMPDIR/hqPos.vcf
+bgzip $TEMPDIR/hqPos.vcf
 if [ $? -gt 0 ]; then
   echo "$script: ERROR with bgzip"
-  rm -rvf $TMPDIR;
+  rm -rvf $TEMPDIR;
   exit 1;
 fi
-tabix $TMPDIR/hqPos.vcf.gz
+tabix $TEMPDIR/hqPos.vcf.gz
 if [ $? -gt 0 ]; then
   echo "$script: ERROR with tabix"
-  rm -rvf $TMPDIR;
+  rm -rvf $TEMPDIR;
   exit 1;
 fi
 
-mv -v $TMPDIR/hqPos.vcf.gz $OUT
-mv -v $TMPDIR/hqPos.vcf.gz.tbi $OUT.tbi
+mv -v $TEMPDIR/hqPos.vcf.gz $OUT
+mv -v $TEMPDIR/hqPos.vcf.gz.tbi $OUT.tbi
 
-rm -rvf $TMPDIR;
+rm -rvf $TEMPDIR;
 
 exit 0;
-
-
-# zcat out.pooled.vcf.gz|perl -lane 'if(/^#/){print;} elsif($F[4] ne "N"){ print; } else { $numF=@F; $has_hq=0; for my $info(@F[8..$numF-1]){ $gt=substr((split(/:/,$info))[0],0,1); $has_hq=1 if($gt=~/[ATCG0\.,]/i);} print if($has_hq); }' | grep -cv '#'
-
-# This command does the following:
-# 1. bcftools merge (merging VCF samples into one)
-# 2. Checks to see if the site is high-quality or not
-#   a. Is it a header or is there no ambiguous alt call? -- print.
-#   b. Is there at least one sample with an unambiguous base call? --print.
-#   c. Are all bases ambiguous? -- skip
-#command="bcftools merge $IN --force-samples | \
-#perl -lane 'if(/^#/){print;} elsif(\$F[4] ne \"N\"){ print; } else { \$numF=@F; \$has_hq=0; for my \$info(@F[9..\$numF-1]){ \$gt=substr((split(/:/,\$info))[0],0,1); \$has_hq=1 if(\$gt=~/[ATCG0\.,]/i);} print if(\$has_hq); }' |\
-#bgzip -c > $OUT.tmp
-#"
-#eval $command
-if [ $? -gt 0 ]; then
-  echo -e "ERROR with bcftools:\n  $command";
-  rm -vf $OUT.tmp
-  exit 1;
-fi;
-mv -v $OUT.tmp $OUT
-if [ $? -gt 0 ]; then
-  echo "ERROR moving $OUT.tmp to $OUT";
-  exit 1;
-fi;
-
-tabix $OUT
-if [ $? -gt 0 ]; then
-  echo "ERROR with tabix"
-  exit 1;
-fi;
 
