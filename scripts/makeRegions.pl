5b1eff227749177a8124c19822ad12e4d83dfbd8
Lee Katz - Aspen <gzu2@cdc.gov>
2015-08-12 09:46:23 -0400

    starting to get ready to read fasta files too

--- scripts/makeRegions.pl
+++ scripts/makeRegions.pl
@@ -4,8 +4,14 @@ use strict;
 use warnings;
 use Data::Dumper;
 use Getopt::Long;
-use File::Basename;
+use File::Basename qw/fileparse basename/;
 use Bio::Perl;
+
+use FindBin;
+use lib "$FindBin::RealBin/../lib";
+use LyveSET qw/logmsg @bamExt @fastaExt/;
+my $fastaExtRegex=join('$|',@fastaExt).'$';
+my $bamExtRegex=join('$|',@bamExt).'$';
 
 local $0=fileparse $0;
 sub logmsg{print STDERR "$0: @_\n";}
@@ -17,16 +23,24 @@ sub main{
   die usage() if($$settings{help});
   $$settings{chunksize}||=10000;
 
-  my $bam=$ARGV[0];
-  die "ERROR: need bam file!\n".usage() if(!$bam);
+  my $infile=$ARGV[0];
+  die "ERROR: need infile file!\n".usage() if(!$infile);
 
-  my $contigLength=lengths($bam,$settings);
+  my $contigLength={};
+  my($name,$path,$suffix)=fileparse($infile,@bamExt,@fastaExt);
+  if($suffix=~/$bamExtRegex/){
+    $contigLength=bamLengths($infile,$settings);
+  } elsif($suffix=~/$fastaExtRegex/){
+    ...;
+  } else {
+    die "ERROR: I do not understand extension $suffix";
+  }
   printChunks($contigLength,$settings);
 
   return 0;
 }
 
-sub lengths{
+sub bamLengths{
   my($sorted,$settings)=@_;
   # Find the total length of the reference genome
   logmsg "Finding the total length of each contig";
@@ -64,7 +78,9 @@ sub printChunks{
 
 sub usage{
   "Creates a list of regions of a bam file, suitable for samtools and bcftools
-  Usage: $0 file.sorted.bam > regions.txt
+  Usage: $0 infile > regions.txt
+  infile must either have an extension of either .bam or .fasta
+
   --chunksize  10000  The size of each region
   "
 }
