0d4db59d6d1f3f774bc04392c88c500a09c7ef27
Lee Katz - Aspen <gzu2@cdc.gov>
2015-08-13 12:50:22 -0400

    smart merging on 'all' and better logmsg

--- scripts/mergeVcf.sh
+++ scripts/mergeVcf.sh
@@ -2,6 +2,8 @@
 # Uses bcftools to merge vcf files
 
 script=$(basename $0);
+bindir=$(dirname $0);
+export PATH=$bindir:$PATH; # enforce the Lyve-SET path
 
 NUMCPUS=1; # default number of cpus
 while getopts "o:n:t:r:s" o; do
@@ -14,10 +16,6 @@ while getopts "o:n:t:r:s" o; do
       ;;
     t)
       TEMPDIR="$OPTARG"
-      ;;
-    r)
-      REGIONFILE="$OPTARG"
-      REGION=$(cat $REGIONFILE);
       ;;
     s)
       ALSOSNPS=1
@@ -33,14 +31,29 @@ IN="$@";
 if [ "$IN" == "" ] || [ "$OUT" == "" ]; then
   echo "$script: merges VCF files that are compressed with bgzip and indexed with tabix"
   echo "Usage: $script -o pooled.vcf.gz *.vcf.gz"
-  echo "  -o output.vcf.gz  The output compressed VCF file"
-  echo "  -t tempdir        A temporary directory (Default: a random dir using mktemp)"
-  echo "  -r regions.txt    A regions file with samtools-style regions (helps with multithreading)"
-  echo "  -s                Output SNPs only file in addition to the whole thing"
+  echo "  -o output.vcf.gz         The output compressed VCF file"
+  echo "  -t /tmp/mergeVcfXXXXXX   A temporary directory that will be completely removed upon exit"
+  echo "  -s                       Output SNPs only file in addition to the whole thing"
   exit 1;
 fi
 
-SNPSOUT=$(basename $OUT .vcf.gz)".snps.vcf.gz"
+SNPSOUT=$(dirname $OUT)/$(basename $OUT .vcf.gz)".snps.vcf.gz"
+
+if [ "$TEMPDIR" == "" ]; then
+  TEMPDIR=$(mktemp -d /tmp/mergeVcf.XXXXXX);
+fi
+mkdir -pv "$TEMPDIR"; # just in case
+
+# Run makeRegions.pl based on how many cpus there are 
+# and really parallelize this script!
+echo "$script: Dividing the genome into regions, making it easier to multithread"
+makeRegions.pl --numchunks $NUMCPUS $IN > $TEMPDIR/regions.txt
+if [ $? -gt 0 ]; then 
+  echo "$script: ERROR running makeRegions.pl!";
+  rm -rvf $TEMPDIR;
+  exit 1;
+fi
+REGION=$(cat $TEMPDIR/regions.txt);
 
 # Figure out regions names using the VCF index files.
 # Do not multithread xargs here because of race conditions and stdout.
@@ -48,16 +61,13 @@ if [ "$REGION" == "" ]; then
   REGION=$(echo "$IN" | xargs -P 1 -n 1 tabix -l | sort | uniq);
 fi
 
-# Multithread, one thread per region
-if [ "$TEMPDIR" == "" ]; then
-  TEMPDIR=$(mktemp -d /tmp/mergeVcf.XXXXXX);
-fi
-mkdir -pv "$TEMPDIR"; # just in case
 
+# Multithread, one thread per region
 echo "$script: temporary directory is $TEMPDIR";
 echo "$script: Running bcftools merge";
 export IN;
-echo "$REGION" | xargs -P $NUMCPUS -n 1 -I {} bash -c 'echo "'$script': merging SNPs in {}"; bcftools merge --apply-filters isIndel --regions "{}" --force-samples -o '$TEMPDIR'/merged.$$.vcf $IN;'
+export script;
+echo "$REGION" | xargs -P $NUMCPUS -n 1 -I {} bash -c 'echo "$script: merging SNPs in {}"; out='$TEMPDIR'/merged.$$.vcf; bcftools merge --merge all --regions "{}" --force-samples -o $out $IN && bgzip $out && tabix $out.gz && echo "$script: finished with region {}";'
 if [ $? -gt 0 ]; then
   echo "$script: ERROR with bcftools merge"
   rm -rvf $TEMPDIR;
@@ -65,7 +75,7 @@ if [ $? -gt 0 ]; then
 fi
 
 echo "$script: Concatenating vcf output"
-bcftools concat $TEMPDIR/merged.*.vcf | vcf-sort > $TEMPDIR/concat.vcf
+bcftools concat --allow-overlaps --remove-duplicates $TEMPDIR/merged.*.vcf.gz > $TEMPDIR/concat.vcf
 if [ $? -gt 0 ]; then
   echo "$script: ERROR with bcftools concat"
   rm -rvf $TEMPDIR;
@@ -75,7 +85,7 @@ fi
 # Generate a SNPs-only merged file, if requested
 if [ "$ALSOSNPS" == 1 ]; then
   echo "$script: parsing $TEMPDIR/concat.vcf for SNPs"
-  bcftools annotate --include '%TYPE="snp"' < $TEMPDIR/concat.vcf > $TEMPDIR/hqPos.vcf
+  bcftools annotate --include '%TYPE="snp"' $TEMPDIR/concat.vcf > $TEMPDIR/hqPos.vcf
   if [ $? -gt 0 ]; then
     echo "$script: ERROR with bcftools annotate"
     rm -rvf $TEMPDIR;
