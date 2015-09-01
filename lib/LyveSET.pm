f2e7981e4cd4ce9c4f6e425221961ca5ce613d02
Lee - spacestation4.cdc.gov <gzu2@cdc.gov>
2015-09-01 09:45:57 -0400

    gbf as a genbank suffix

--- lib/LyveSET.pm
+++ lib/LyveSET.pm
@@ -28,7 +28,7 @@ our @fastqExt=qw(.fastq.gz .fastq .fq .fq.gz);
 our @fastaExt=qw(.fasta .fna .faa .mfa .fas .fa);
 our @bamExt=qw(.sorted.bam .bam);
 our @vcfExt=qw(.vcf.gz .vcf);
-our @richseqExt=qw(.gbk .gb .embl);
+our @richseqExt=qw(.gbk .gbf .gb .embl);
 
 #################################################
 ### COMMON SUBS/TOOLS (not object subroutines) ##
