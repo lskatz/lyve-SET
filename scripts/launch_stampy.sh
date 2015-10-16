2657c3f587bac857a3a9a54d9acb4b4e17f62f6c
Lee Katz - Aspen <gzu2@cdc.gov>
2015-10-16 12:08:47 -0400

    removed -k option in gunzip

--- scripts/launch_stampy.sh
+++ scripts/launch_stampy.sh
@@ -80,7 +80,7 @@ done
 
 # Input file requirements and dependency check
 [[ -z "$IN_FASTQ" || -z "$REFERENCE" ]] && { usage; exit 1; }
-[[ "$IN_FASTQ" == *.gz ]] && gunzip -k "$TMP"/"$IN_FASTQ" && IN_FASTQ="$TMP"/"$IN_FASTQ"
+[[ "$IN_FASTQ" == *.gz ]] && gunzip "$TMP"/"$IN_FASTQ" && IN_FASTQ="$TMP"/"$IN_FASTQ"
 [[ ! -s "$REFERENCE.stidx" || ! -s "$REFERENCE.sthash" ]] && { echo 'ERROR: hash and/or index file not found'; exit 1; }
 command -v stampy.py >/dev/null 2>&1 || { echo 'ERROR: stampy.py not found' >&2; exit 1; }
 
