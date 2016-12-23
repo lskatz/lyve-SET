d5662bdcc39385a2550e9799cf4920746c38a97d
Lee Katz - Aspen <gzu2@cdc.gov>
2016-12-23 12:00:46 -0500

    trap statement to remove tempdir; numcpus is in usage; logmsg function

--- scripts/set_mergeVcf.sh
+++ scripts/set_mergeVcf.sh
@@ -5,6 +5,10 @@ script=$(basename $0);
 bindir=$(dirname $0);
 export PATH=$bindir:$PATH; # enforce the Lyve-SET path
 
+function logmsg(){
+  echo "$script: $@"
+}
+
 NUMCPUS=1; # default number of cpus
 while getopts "o:n:t:r:s" o; do
   case "${o}" in
@@ -21,7 +25,8 @@ while getopts "o:n:t:r:s" o; do
       ALSOSNPS=1
       ;;
     *)
-      echo "ERROR: I do not understand option $OPTARG"
+      logmsg "ERROR: I do not understand option --$o $OPTARG"
+      exit 1
       ;;
   esac
 done
@@ -34,6 +39,7 @@ if [ "$IN" == "" ] || [ "$OUT" == "" ]; then
   echo "  -o output.vcf.gz         The output compressed VCF file"
   echo "  -t /tmp/mergeVcfXXXXXX   A temporary directory that will be completely removed upon exit"
   echo "  -s                       Output SNPs only file in addition to the whole thing"
+  echo "  -n 1                     Number of cpus"
   exit 1;
 fi
 
@@ -41,59 +47,49 @@ SNPSOUT=$(dirname $OUT)/$(basename $OUT .vcf.gz)".snps.vcf.gz"
 
 if [ "$TEMPDIR" == "" ]; then
   TEMPDIR=$(mktemp -d /tmp/mergeVcf.XXXXXX);
+  trap "rm -rvf $TEMPDIR" EXIT TERM INT
 fi
 mkdir -pv "$TEMPDIR"; # just in case
+logmsg "temporary directory is $TEMPDIR";
 
 # Run makeRegions.pl based on how many cpus there are 
 # and really parallelize this script!
-echo "$script: Dividing the genome into regions, making it easier to multithread"
+logmsg "Dividing the genome into regions, making it easier to multithread"
 makeRegions.pl --numchunks $NUMCPUS $IN > $TEMPDIR/regions.txt
 if [ $? -gt 0 ]; then 
-  echo "$script: ERROR running makeRegions.pl!";
-  rm -rvf $TEMPDIR;
+  logmsg "ERROR running makeRegions.pl!";
   exit 1;
 fi
 REGION=$(cat $TEMPDIR/regions.txt);
 
-# Figure out regions names using the VCF index files.
-# Do not multithread xargs here because of race conditions and stdout.
-if [ "$REGION" == "" ]; then 
-  REGION=$(echo "$IN" | xargs -P 1 -n 1 tabix -l | sort | uniq);
-fi
-
-
 # Multithread, one thread per region
-echo "$script: temporary directory is $TEMPDIR";
-echo "$script: Running bcftools merge";
+logmsg "Running bcftools merge";
 export IN;
 export script;
+# TODO if we upgrade past v1.3.1, add in the parameter '--filter-logic x'
 echo "$REGION" | xargs -P $NUMCPUS -n 1 bash -c 'echo "$script: merging SNPs in $0"; out='$TEMPDIR'/merged.$$.vcf; bcftools merge --merge all --regions "$0" --force-samples -o $out $IN && bgzip $out && tabix $out.gz && echo "$script: finished with region $0";'
 if [ $? -gt 0 ]; then
-  echo "$script: ERROR with bcftools merge"
-  rm -rvf $TEMPDIR;
+  logmsg "ERROR with bcftools merge"
   exit 1;
 fi
 
-echo "$script: Concatenating vcf output"
-bcftools concat $TEMPDIR/merged.*.vcf.gz > $TEMPDIR/concat.vcf
-#bcftools concat --allow-overlaps --remove-duplicates $TEMPDIR/merged.*.vcf.gz > $TEMPDIR/concat.vcf
+logmsg "Concatenating vcf output"
+bcftools concat --allow-overlaps --remove-duplicates $TEMPDIR/merged.*.vcf.gz > $TEMPDIR/concat.vcf
 if [ $? -gt 0 ]; then
-  echo "$script: ERROR with bcftools concat"
-  rm -rvf $TEMPDIR;
+  logmsg "ERROR with bcftools concat"
   exit 1;
 fi
 
 # Generate a SNPs-only merged file, if requested
 if [ "$ALSOSNPS" == 1 ]; then
-  echo "$script: parsing $TEMPDIR/concat.vcf for SNPs"
-  bcftools annotate --include '%TYPE="snp"' $TEMPDIR/concat.vcf > $TEMPDIR/hqPos.vcf
+  logmsg "parsing $TEMPDIR/concat.vcf for SNPs"
+  bcftools view --include '%TYPE="snp"' $TEMPDIR/concat.vcf > $TEMPDIR/hqPos.vcf
   if [ $? -gt 0 ]; then
-    echo "$script: ERROR with bcftools annotate"
-    rm -rvf $TEMPDIR;
+    logmsg "ERROR with bcftools view"
     exit 1;
   fi
 else
-  echo "$script: user did not request SNPs-only file also"
+  logmsg "user did not request SNPs-only file also"
 fi
 
 # BGzip, tabix, and mv hqSNPs and concat vcfs
@@ -104,14 +100,12 @@ for VCF in $TEMPDIR/concat.vcf $TEMPDIR/hqPos.vcf; do
 
   bgzip $VCF
   if [ $? -gt 0 ]; then
-    echo "$script: ERROR with bgzip $VCF"
-    rm -rvf $TEMPDIR;
+    logmsg "ERROR with bgzip $VCF"
     exit 1;
   fi
   tabix $VCF.gz
   if [ $? -gt 0 ]; then
-    echo "$script: ERROR with tabix $VCF.gz"
-    rm -rvf $TEMPDIR;
+    logmsg "ERROR with tabix $VCF.gz"
     exit 1;
   fi
 done
@@ -120,15 +114,13 @@ done
 if [ "$ALSOSNPS" == 1 ]; then
   mv -v $TEMPDIR/hqPos.vcf.gz $SNPSOUT
   mv -v $TEMPDIR/hqPos.vcf.gz.tbi $SNPSOUT.tbi
-  echo "$script: SNPs-only file can be found in $SNPSOUT";
+  logmsg "SNPs-only file can be found in $SNPSOUT";
 fi
 
 # Lastly, mv over the large file
 mv -v $TEMPDIR/concat.vcf.gz $OUT
 mv -v $TEMPDIR/concat.vcf.gz.tbi $OUT.tbi
-echo "$script: Output file can be found in $OUT"
-
-rm -rvf $TEMPDIR;
+logmsg "Output file can be found in $OUT"
 
 exit 0;
 
