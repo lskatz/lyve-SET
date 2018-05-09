cdf00caad73dbb7440c3bb36a0893ee41e3bbcfb
Lee Katz - Aspen <gzu2@cdc.gov>
2018-05-09 09:05:15 -0400

    fixed percentage of gt 30 reads mapped and vcf table

--- plugins/diagnosis/diagnose_v2.pl
+++ plugins/diagnosis/diagnose_v2.pl
@@ -17,6 +17,11 @@ use LyveSET qw/logmsg/;
 # Bring in all script dependencies here
 $ENV{PATH} = "$ENV{PATH}:$RealBin/../../scripts";
 
+# Global variables
+my @maskingHeaders=qw(alignment ambiguous_base_count ambiguous_base_percent GC% contig_length);
+my @bamHeaders=qw(bam total_reads mapped_reads mapped_reads_ge30 unmapped_reads percent_mapped percent_mapped_gt30 proper_pairs inward_pairs outward_pairs pairs_with_other_orientation percentage_proper_pairs insert_size_avg number_sites_with_coverage_lt20 breadth_coverage total_sites_covered_ge20 avg_coverage_per_site);
+my @vcfHeaders=qw(vcf PASS DP RF AF isIndel masked str10 indelError);
+
 # Run the main subroutine, and the exit code for the script
 # is the return integer from main().
 exit(main());
@@ -27,6 +32,7 @@ sub main{
   die usage() if($$settings{help});
   my $project = $ARGV[0];
   die "ERROR: need project directory\n".usage() if(!$project);
+  $project=~s/\/+$//; # trim off any leading slashes for readability
 
   #.....Setting the paths to fetch files!.........#
   my $path_cleaned_reads = "$project/reads";
@@ -36,7 +42,11 @@ sub main{
   my $out_info_path      = "$project/msa/out.informative.fasta";
   my $reference          = "$project/reference/reference.fasta";
 
-  # the reference size         
+  if(!-e $reference){
+    die "ERROR: could not find reference file $reference";
+  }
+
+  # the reference size 
   logmsg "Reading the reference genome";
   my $total_ref_bases = gsize($reference);
 
@@ -46,18 +56,35 @@ sub main{
   my $maskingAlnHash  = masking($out_aln_path);
 
   # Print the masking report
-  print join("\t",qw(full_alignment ambiguous_base_count ambiguous_base_percent GC% contig_length))."\n";
+  print "# Full alignment\n";
+  print join("\t",@maskingHeaders)."\n";
   for my $m (@$maskingAlnHash) {
     print join("\t", $$m{id}, $$m{countN}, $$m{percentN}, $$m{percentGC}, $$m{length})."\n";
   }
   print "\n";
-  print join("\t",qw(informative_alignment ambiguous_base_count ambiguous_base_percent GC% contig_length))."\n";
+  print "# Informative alignment\n";
+  print join("\t",@maskingHeaders)."\n";
   for my $m (@$maskingInfoHash) {
     print join("\t", $$m{id}, $$m{countN}, $$m{percentN}, $$m{percentGC}, $$m{length})."\n";
   }
 
+  # Print the bam report
+  print "# BAM report\n";
+  print join("\t",@bamHeaders)."\n";
   foreach my $bam(glob("$bam_path/*.bam")){
+    logmsg "Processing bam $bam...";
     my $bamInfo = bamInfo($bam,$path_cleaned_reads,$total_ref_bases,$vcf_path);
+    print $$bamInfo{$_}."\t" for(@bamHeaders);
+    print "\n";
+  }
+
+  # VCF report
+  print "# VCF report\n";
+  print join("\t",@vcfHeaders)."\n";
+  foreach my $vcf(glob("$vcf_path/*.vcf.gz")){
+    my $vcfInfo = vcfInfo($vcf,$path_cleaned_reads,$total_ref_bases,$vcf_path);
+    print $$vcfInfo{$_}."\t" for(@vcfHeaders);
+    print "\n";
   }
 
   return 0;
@@ -65,87 +92,110 @@ sub main{
 
 sub bamInfo{
   my($bam,$path_cleaned_reads,$total_ref_bases,$vcf_path)=@_;
+
+  # Default return hash
+  my %bamInfo=(bam=>$bam); 
+  for my $field (@bamHeaders){
+    $bamInfo{$field}//=".";
+  }
+
   # Example filename: bam/sample1.fastq.gz-reference.sorted.bam
   # Example basename: sample1
   my $basename = basename($bam,qw(.fastq.gz-reference.sorted.bam));
-			   
-  print ("\n------------------------------------------------------------\n");
-  print "\n\n\n---> Processing file: $basename...\n";
-  print ("\n\n===================== QC on BAM File ===============\n");
+  $bamInfo{bam}=$basename;
 
-  my $total_mapped_reads = `samtools view -c -F 4 $bam`; #Fetching total number of mapped reads  #NOTE: saving system() output to perl variable, returns 0; it sets the exit status to perl variable; Hence use ``
-  my $mapped_reads = `samtools view -q 30 $bam | wc -l`;  #Fetching number of mapped reads with quality higher than 30
-  my $mapped_reads_lt_30 = $total_mapped_reads - $mapped_reads; #Number of reads < Q30
+  #Fetching total number of mapped reads
+  #NOTE: saving system() output to perl variable, returns 0
+  #it sets the exit status to perl variable; Hence use ``
+  my $total_mapped_reads = `samtools view -c -F 4 $bam` + 0; # cast to int; remove newline
+  die if $?;
+  #Fetching number of mapped reads with quality greater than or equal to 30
+  my $mapped_reads_ge_30 = `samtools view -q 30 $bam | wc -l` + 0;
+  die if $?;
+  #Number of reads < Q30
+  my $mapped_reads_lt_30 = $total_mapped_reads - $mapped_reads_ge_30;
+
+  $bamInfo{mapped_reads}     =$total_mapped_reads + 0;
+  $bamInfo{mapped_reads_ge30}=$mapped_reads_ge_30 + 0;
+  $bamInfo{mapped_reads_lt30}=$mapped_reads_lt_30 + 0;
                
   #Calculating total number of reads from clean_reads files
-  my $file = "$path_cleaned_reads"."/"."$basename*gz";          
-  my $line_count= `zcat $file | wc -l`;   #gettting total number of lines in fastq files
-  my $tot_reads = $line_count/4;          #Dividing total number of lines by 4, to get total number of reads
-  my $not_mapped = $tot_reads - $total_mapped_reads; # Calculating number of reads that are not mapped
-  my $perc_mapped_reads = ($total_mapped_reads/$tot_reads) * 100; #Calculating percentage of mapped reads
-  my $perc_mapped_reads_gt_30 = ($mapped_reads/$tot_reads) * 100; #Calculating percentage of mapped reads > Q30
+  # Gather hopefully just one file pertaining to this bam
+  my @file = glob("$path_cleaned_reads"."/"."$basename*fastq*");
+  if(@file > 1){
+    logmsg "Warning: found more than one fastq file for $bam";
+  } elsif(@file < 1){
+    logmsg "Warning: could not find a fastq file for $bam";
+    return \%bamInfo;
+  }
+  my $file = join(" ",@file);
+  #Gettting total number of lines in fastq files: the
+  #number of lines divided by 4.
+  my $line_count= `zcat $file | wc -l`;
+  die if $?;
+  my $tot_reads = $line_count/4;
+  $bamInfo{total_reads}=$tot_reads;
+
+  $bamInfo{unmapped_reads} = $tot_reads - $total_mapped_reads; # Calculating number of reads that are not mapped
+  $bamInfo{percent_mapped} = ($total_mapped_reads/$tot_reads) * 100; #Calculating percentage of mapped reads
+  $bamInfo{percent_mapped_gt30} = ($mapped_reads_ge_30/$total_mapped_reads) * 100; #Calculating percentage of mapped reads > Q30
 			 
-  print "\nTotal number of reads in File -> $basename: $tot_reads\n";  # Printing total number of reads
-  print "Total number of mapped reads: $total_mapped_reads";
-  print "Number of Reads Mapped > Q30: $mapped_reads";
-  print "Number of Reads mapped < Q30: $mapped_reads_lt_30\n";
-  print "Number of Reads not mapped: $not_mapped\n";
-  print "Percent mapped reads: $perc_mapped_reads\n";
-  print "Percent mapped reads > Q30: $perc_mapped_reads_gt_30\n";
+  #my @bamHeaders=qw(bam total_reads mapped_reads mapped_reads_gt30 unmapped_reads percent_mapped percent_mapped_gt30 proper_pairs inward_pairs outward_pairs pairs_with_other_orientation percentage_proper_pairs insert_size_avg number_sites_with_coverage_lt20 breadth_coverage total_sites_covered_ge20 avg_coverage_per_site);
 
-  my $str1 = `samtools stats $bam | head -37 | grep 'reads properly paired:'`;
-  $str1 =~s/^SN\s+//;        #Formatting output
-  $str1 =~s/#.*//;
-  print $str1;                         #reads properly paired:  ###### is stored as one string. Inorder to get the number, Splitting at ':'
+  # Run samtools one time and fill in some more variables
+  my $stats_line_counter=0;
+  open(SAMTOOLS_STATS, "samtools stats $bam | ") or die "ERROR running samtools stats $bam: $!";
+  while(<SAMTOOLS_STATS>){
+    last if($stats_line_counter++ > 40);
+    chomp;
 
-  my @temp = split ":", $str1; 
-  my $proper_paired = $temp[1];        #second element of the array is the number
-  $proper_paired =~s/\s+//;           #Removing the white spaces before the number
-  my $perc_proper_paired = ($proper_paired/$mapped_reads) * 100;
+    if     (/reads properly paired:\s*(\d+)/){
+      $bamInfo{proper_pairs}=$1;
+      $bamInfo{percentage_proper_pairs}=sprintf("%0.2f",$bamInfo{proper_pairs}/$bamInfo{mapped_reads_ge30} * 100);
+    } elsif(/inward oriented pairs:\s*(\d+)/){
+      $bamInfo{inward_pairs}=$1;
+    } elsif(/outward oriented pairs:\s*(\d+)/){
+      $bamInfo{outward_pairs}=$1;
+    } elsif(/pairs with other orientation:\s*(\d+)/){
+      $bamInfo{pairs_with_other_orientation}=$1;
+    } elsif(/insert size average:\s*(\d+)/){
+      $bamInfo{insert_size_avg}=$1;
+    }
+  }
+  close SAMTOOLS_STATS;
 
+  # Percentage of coverage of the reference genome with depth >=20
+  my $sites_lt20=0;
+  my $sites_ge20=0;
+  my $coverage_count=0;
+  my $bam_sites=0;
+  open(SAMTOOLS_DEPTH, "samtools depth $bam |") or die "ERROR running samtools depth $bam: $!";
+  while(<SAMTOOLS_DEPTH>){
+    chomp;
+    my($chr,$pos,$depth)=split(/\t/,$_);
+    $coverage_count+=$depth;
+    $bam_sites++;
+    if($depth >= 0 && $depth < 20){
+      $sites_lt20++;
+    } else {
+      $sites_ge20++;
+    }
+  }
+  close SAMTOOLS_DEPTH;
+  $bamInfo{number_sites_with_coverage_lt20}=$sites_lt20;
+  $bamInfo{total_sites_covered_ge20}=$sites_ge20;
+  $bamInfo{breadth_coverage}=$bam_sites;
+  $bamInfo{avg_coverage_per_site}=sprintf("%0.2f",$coverage_count/$total_ref_bases);
 
+  return \%bamInfo;
+}
 
-  my $str2 = `samtools stats $bam | head -37 | grep 'inward oriented pairs:'`;
-  $str2 =~s/^SN\s+//;                        #Formatting output
-  print $str2;
-
-  my $str3 = `samtools stats $bam | head -37 | grep 'outward oriented pairs:'`;
-  $str3 =~s/^SN\s+//;                        #Formatting output
-  print $str3;
-
-  my $str4 = `samtools stats $bam | head -37 | grep 'pairs with other orientation:'`;
-  $str4 =~s/^SN\s+//;                        #Formatting output
-  print $str4;
-
-
-  print "Percentage of properly paired reads = $perc_proper_paired\n";
-
-
-  my $str5 = `samtools stats $bam | head -37 | grep 'insert size average:'`;
-  $str5 =~s/^SN\s+//;                        #Formatting output
-  print $str5;
-
-
-  my $baseslt20 = `samtools depth $bam | awk '( (\$3 >= 0) && (\$3 <= 19) )'| wc -l`;
-  print "Number of bases with mapping coverage less than 20: $baseslt20";
-
-
-  my $perc_reference_coverage = ((($total_ref_bases - $baseslt20)/$total_ref_bases)*100);
-  print "Percent reference coverage = $perc_reference_coverage\n";
-
-
-
-  my $tot_coverage_bases = `samtools depth $bam | cut -f 3| awk '{sum += \$_} END {print sum}'`;  # Total coverage of all bases
-  print "Total coverage of bases (based off 20x at each position) = $tot_coverage_bases";
-
-  my $average_coverage = $tot_coverage_bases/$total_ref_bases;
-  print "Average Coverage = $average_coverage";
+sub vcfInfo{
+  my($vcf,$path_cleaned_reads,$total_ref_bases,$vcf_path)=@_;
+  my $basename = basename($vcf,qw(-reference.vcf.gz));
 
 
-  print "\n\n\n=============================== QC on VCF File =========================\n";
-  my $vcf_filename= "$vcf_path"."/".$basename."*.gz";
-
-
+=cut
   print("Filters applied for sites that do not pass -
   a. DP20 = Depth is less than 20, the user-set coverage threshold
   b. RF0.95 = Reference variant consensus is less than 0.95, the user-set threshold
@@ -154,10 +204,25 @@ sub bamInfo{
   e. masked = This site was masked using a bed file or other means
   f. str10 = Less than 10% or more than 90% of variant supporting reads on one strand
   g. indelError = Likey artifcat due to indel reads at this position\n\n");
-               
-  my $filter_codes=`zcat $vcf_filename | grep -v '^#' | cut -f 7 | sort | uniq -c`;
-  print("$filter_codes");
+=cut
 
+  # set defaults
+  my %filter_code = (vcf=>$basename);
+  for(@vcfHeaders){
+    $filter_code{$_}//=".";
+  }
+
+  open(VCF,"zcat $vcf | ") or die "ERROR: could not zcat $vcf: $!";
+  while(<VCF>){
+    next if(/^#/);
+    chomp;
+
+    my(@F) = split(/\t/);
+    $filter_code{$F[6]}++;
+  }
+  close VCF;
+
+  return \%filter_code;
 }
 
 
@@ -205,10 +270,6 @@ sub masking() {
     );
   }
 
-  #Declare a new array for the sorting the unsorted array. Use $$ to reference the other array. 
-  #Sort the array within an array on $num (the second element in the unsorted array) in ascending order (a <=> b) on GC content 
-  #my @sorted= sort {$$a[2] <=> $$b[2]} @unsorted;
-               
   #Loop through the array.. Print the contig sequences in order of GC content (lowest to highest)
   my @sorted_id = sort{ $$a{percentGC} <=> $$b{percentGC} } @unsorted_id;
   return \@sorted_id;
@@ -233,17 +294,9 @@ sub gsize(){
   #write each entry in the input file to STDOUT, while the condition is true
   #seq_in is the object that holds the file
   #next seq passes each contig to the sequence object $seq
-  while (my $seq = $seq_in->next_seq ()) 
-                { 
-
-  #Initialize variables; start the counter at 0 for each contig that comes through the loop  
-
-  $id= $seq->id; #Get the header Id of each contig
-  $length +=$seq->length;  #Declare variable for finding the length of each contig sequence
-
-  $string = $seq->seq;   #Each contig seq is treated as a string
-
-
+  while (my $seq = $seq_in->next_seq ()) { 
+    #Initialize variables; start the counter at 0 for each contig that comes through the loop  
+    $length +=$seq->length;  #Declare variable for finding the length of each contig sequence
   }
 
   return $length;
@@ -255,3 +308,4 @@ sub usage{
   Usage: ./hqSNP_diagnose.pl projectdir
   "
 }
+
