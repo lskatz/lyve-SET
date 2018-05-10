3b693383baa13979dff81e08e2c083511785c640
Lee Katz - Aspen <gzu2@cdc.gov>
2018-05-10 11:57:06 -0400

    m

--- scripts/testScripts.sh
+++ scripts/testScripts.sh
@@ -13,6 +13,7 @@ for i in $testsDir/*.sh; do
 
   # While running the program, indent any output.
   bash $i 2>&1 | perl -lane 'chomp; print "  $_";'
+  # Checking the exit code is bash-specific and so the #! line is important
   exit_code=${PIPESTATUS[0]}
 
   # If there is any failure, mark it and move on
